1045993dad80e6c3749998c4aee866531bd67ecb
shoxjahon2009 <shohjaxondehqonboyev59@gmail.com>
2024-02-17 13:33:16 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,70 @@
+
+## 1 masala
+# a = [1,2,3,4,5,6,7,8,9,10]
+# b = list(enumerate(a))
+# print(b)
+
+## 2-masala
+# a = [1,2,3,4,5,6,7,8,9,777]
+# b = max(a)
+# print(b)
+
+### 3masala
+# b = int(input("son:"))
+# a = [1,2,3,4,5]
+# for x in a:
+#     if b == x:
+#         a.remove(b)
+#         a.append(b)
+# print(a)
+
+## 4 masala
+# a = input("Enter")
+# if a == " I DON'T WANT MEN ONLINE":
+#     print("no one online❌❌")
+# elif a == "I'M ONLINE":
+#     print("user1 online👌👌")
+# elif a == "ME AND MY FRIEND ARE ONLINE TOGETHER":
+#     print("user1 and friends onlin❗❗")
+
+## 5masala
+
+# a = [1,2,]
+# b = []
+# for x in a:
+#     b.copy()
+#     b.append(a)
+# print(b)
+### 6 masasla
+# a = [3,4,5,7,8,17,9,2]
+# b = []
+# for x in a:
+#     if 6 < x:
+#         b.append(x)
+# print(b)
+###  7 masala
+# a = [1,2,3,4,"a",6,"s"]
+# b = []
+# for x in a:
+#     if type(x) == int:
+#         b.append(x)
+# print(b)
+###  8 masala
+# names = ["Adam", "Sarah", "Malcolm"]
+# table = []
+# for name in names:
+#     table.append(name[0])
+#     table.sort()
+# j = "".join(table)
+# print(j)
+              ## 8-masalani 2 chi bosqichi❗❗
+#
+# names = ["Harry", "Newt", "Luna", "Cho"]
+# name = []
+# for x in names:
+#     name.append(x[0])
+#     name.sort()
+# a = "".join(name)
+# print(a)
+
+
